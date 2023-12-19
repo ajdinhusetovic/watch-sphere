fe0a87be770de9a1adafd7b231bfcba9752d8791
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-12-19 14:49:10 +0100

    Install project, and create logo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import "../scss/home.scss";
+
+const Home = () => {
+  return (
+    <div className="home-wrapper">
+      <div className="logo-wrapper">
+        <span id="watch">WATCH</span>
+        <span id="sphere">sphere</span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
